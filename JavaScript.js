02c78a2c5e3fe24b1974eb81b0615c8aa412028c
Konrad S <konrad-sosna@wp.pl>
2021-01-06 00:48:31 +0100

    some changes

---
+++ JavaScript.js
@@ -0,0 +1,42 @@
+﻿// hover button animation shake 
+ $(document).ready(function () {
+    $('.button').hover(function () {
+                $(this).addClass('animate__animated animate__headShake');
+            }, function () {
+                $(this).removeClass('animate__animated animate__headShake');
+    });
+  });
+
+// hover letters animation shake
+ $(document).ready(function () {
+    $('span').hover(function () {
+        $(this).addClass('animate__animated animate__headShake');
+            }, function () {
+                $(this).removeClass('animate__animated animate__headShake');
+    });
+ });
+
+// loading bar animation
+    $(function () {
+        $(".meter > span").each(function () {
+            $(this)
+                .data("origWidth", $(this).width())
+                .width(0)
+                .animate({
+                    width: $(this).data("origWidth")
+                }, 1200);
+        });
+    });
+
+// hover letters color red
+$(document).ready(function () {
+    let redletters = document.getElementById("title");
+    redletters.addEventListener("mouseover", function (event) {
+        event.target.style.color = "red";
+    });
+    redletters.addEventListener("mouseout", function (event) {
+        setTimeout(function () {
+            event.target.style.color = "";
+        }, 200);
+    });
+});
