9efb8e31a59af9bda75c1e5f481f0ff6d21edb3f
Konrad S <konrad-sosna@wp.pl>
2021-02-21 15:38:14 +0100

    add caching

---
+++ script/script.js
@@ -0,0 +1,46 @@
+﻿const CACHE_NAME = "audioCache";
+const FILE_NAME = '/logo.webp';
+
+// Find DOM elements
+//const status = document.getElementById("js-status");
+//const cacheButton = document.getElementById("js-cache-btn");
+//const removeCacheButton = document.getElementById("js-remove-cache-btn");
+
+// Hook up event listeners
+window.addEventListener('load', addToCache);
+//removeCacheButton.addEventListener('click', removeFromCache);
+
+(function main() {
+    updateStatus();
+})();
+
+// Update the status field
+function updateStatus() {
+    isCached().then(value => {
+        //status.innerText = '' + value;
+    });
+}
+
+function isCached() {
+    return window.caches.open(CACHE_NAME)
+        .then(cache => cache.match(FILE_NAME))
+        .then(Boolean);
+}
+
+function addToCache() {
+    window.caches.open(CACHE_NAME)
+        .then(cache => cache.add(FILE_NAME))
+        .then(() => console.log('cached pic file'))
+        .catch(e => console.error('failed to cache file', e))
+        .finally(updateStatus); // This only works in chrome/ff at the time of writing
+}
+
+//function removeFromCache() {
+//    window.caches.open(CACHE_NAME)
+//        .then(cache => cache.delete(FILE_NAME))
+//        .then(() => console.log('removed cached file'))
+//        .catch(e => console.error('failed to remove cached file', e))
+//        .finally(updateStatus); // This only works in chrome/ff at the time of writing
+//}
+
+
---
+++ script/sw.js
@@ -0,0 +1,20 @@
+﻿const FILE_NAME = '/script/sw.js';
+
+self.addEventListener('fetch', event => {
+    // Checking if the url matches the audio file.
+    // This is actually not really needed right now, we could just check if the request is
+    // cached (for any file) and return it. If you decide to put the response from the network
+    // fetch into the cache then this check might be valueble to only store the audio files.
+    if (event.request.url.indexOf(FILE_NAME) === 0) {
+        console.log('Audio file requested!')
+
+        event.respondWith(
+            caches.match(event.request).then(function (response) {
+                console.log(Boolean(response) ? 'Serving from cache' : 'Fetching from network');
+                return response || fetch(event.request);
+            })
+        );
+    } else {
+        event.respondWith(fetch(event.request));
+    }
+});
