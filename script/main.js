4fa403a5626c985acde680446702284712cad6f3
Konrad S <konrad-sosna@wp.pl>
2021-02-22 02:15:45 +0100

    add service worker

--- script/main.js
+++ script/main.js
@@ -214,3 +214,13 @@ function linksDelay() {
 
     });
 };
+
+// Make sure sw are supported
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('../sw_cached_pages.js')
+            .then(reg => console.log('Service Worker: Registered (Pages)'))
+            .catch(err => console.log(`Service Worker: Error: ${err}`));
+    });
+}
